7d44c184b6b9b97b4bbe0480615efd4686f2db83
Karthi29M11 <142421611+Karthi29M11@users.noreply.github.com>
2025-06-16 15:06:38 +0530

    Update Task_7_1.java

--- JAVA_TASK_PRACTICE/Task_7_1.java
+++ JAVA_TASK_PRACTICE/Task_7_1.java
@@ -1,24 +1,37 @@
 package com.index;
 //Method OverLoading
 public class Task_7_1 {
-	public int employee1(int id) {
-		return id;
+	private int id,age;
+	private String name,location;
+	Task_7_1(int i,int a){
+		this.id = i;
+		this.age = a;
 	}
-	public int employee2(int age) {
-		return age;
+	Task_7_1(int i,int a,String n){
+		this.id = i;
+		this.age = a;
+		this.name = n;
 	}
-	public String employee3(String name) {
-		return name;
+	Task_7_1(int i,int a,String n,String l){
+		this.id = i;
+		this.age = a;
+		this.name = n;
+		this.location =l;
 	}
-	public String employee4(String location) {
-		return location;
+	void display() {
+		System.out.println("\nThe Id of Employee1 is :" +id);
+		System.out.println("The Age of Employee2 is :" +age);
+		System.out.println("The Name of Employee3 is :" +name);
+		System.out.println("The Location of Employee is :" +location);
 	}
 	public static void main(String[] args) {
-		Task_7_1 emp1 = new Task_7_1();
-		System.out.println("The Id of Employee1 is :" +emp1.employee1(11));
-		System.out.println("The Age of Employee2 is :" +emp1.employee2(23));
-		System.out.println("The Name of Employee3 is :" +emp1.employee3("Rossi"));
-		System.out.println("The Location of Employee is :" +emp1.employee4("Kanchipuram"));
+		Task_7_1 emp = new Task_7_1(111,22,"Rossi","Kanchipuram");
+		Task_7_1 emp1 = new Task_7_1(112,21,"Thiru","Arakkonam");
+		Task_7_1 emp2 = new Task_7_1(113,23,"Sam","Chennai");
+		emp.display();
+		emp1.display();
+		emp2.display();
+
 	}
 	
 
